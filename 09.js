bacf65042ed8a6b256310171ad6cf3cba4241efd
DimaMaslo88 <dmitrij-maslo@mail.ru>
2021-10-09 23:55:10 +0300

    code))

---
+++ 09.js
@@ -0,0 +1,9 @@
+var countSheep = function (num){
+    if(num == 1){
+        return("1 sheep...");
+    }else if(num == 2){
+        return("1 sheep...2 sheep...");
+    }else if (num == 3){
+        return("1 sheep...2 sheep...3 sheep...");
+    }
+}
