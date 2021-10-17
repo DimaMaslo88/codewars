bfa09bf9d1023c5ab8e4acfd557667a95ecf121d
DimaMaslo88 <dmitrij-maslo@mail.ru>
2021-10-17 19:21:16 +0300

    codewars

---
+++ 29.js
@@ -0,0 +1,7 @@
+function areYouPlayingBanjo(name) {
+    if (name.charAt(0) == "R" || name.charAt(0) == "r") {// Implement me
+        return (name + " plays banjo");
+    } else {
+        return (name + " does not play banjo");
+    }
+}
