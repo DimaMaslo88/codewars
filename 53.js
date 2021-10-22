0a17990c5dc34dc18fc16fed85b4271d1435571c
DimaMaslo88 <dmitrij-maslo@mail.ru>
2021-10-22 13:36:47 +0300

    codewars

---
+++ 53.js
@@ -0,0 +1,24 @@
+function getAge(inputString) {
+    let x = inputString;
+    if (x == "1 year old") {
+        return 1;
+    } else if (x == "2 years old") {
+        return 2;
+    } else if (x == "3 years old") {
+        return 3;
+    } else if (x == "4 years old") {
+        return 4;
+    } else if (x == "5 years old") {
+        return 5;
+    } else if (x == "6 years old") {
+        return 6;
+    } else if (x == "7 years old") {
+        return 7;
+
+    } else if (x == "8 years old") {
+        return 8;
+    } else if (x == "9 years old") {
+        return 9;
+    }
+// return correct age (int). Happy coding :)
+}
