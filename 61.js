984792a71b9f8589e0604cc8956aac558780e77a
DimaMaslo88 <dmitrij-maslo@mail.ru>
2021-10-25 12:49:47 +0300

    'codewars'

---
+++ 61.js
@@ -0,0 +1,7 @@
+function hello(name) {
+    if (name) {
+        return 'Hello, ' + name[0].toUpperCase() + name.substring(1, name.length).toLowerCase() + '!';
+    } else {
+        return 'Hello, World!';
+    }
+}
